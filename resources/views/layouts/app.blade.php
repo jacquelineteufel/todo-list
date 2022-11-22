c67af54b91cce1f695c590c38d92d1271e5f867e
jacquelineteufel <jac.teufel.21@lehre.mosbach.dhbw.de>
2022-11-22 14:38:25 +0100

    Fehlerbehebung

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!--Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js','node_modules/bootstrap/dist/css/bootstrap.min.css'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
