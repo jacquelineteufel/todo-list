eff9598ebade6840a84e560c1ac61ce38f76a12d
jacquelineteufel <jac.teufel.21@lehre.mosbach.dhbw.de>
2022-11-25 11:31:11 +0100

    In add.blade und edit.blade bootstrap eingebunden

--- resources/views/todos/add.blade.php
+++ resources/views/todos/add.blade.php
@@ -3,19 +3,38 @@
    <head>
       <meta charset="utf-8">
       <title>Aufgabe erstellen</title>
+      <!-- CSS only -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
    </head>
+
    <body>
-   <body style="text-align:center">
+   <div class="container">
       <h1>Aufgabe erstellen</h1>
       <form action="/upload" method="post">
         @csrf
+        @method('POST')
+
+        <div class="mb-3">
          <label for="exampleInputEmail1">Titel</label>
          <input type="text" class="form-control" name="title"> <br>
+      </div>
+
+      <div class="mb-3">
          <label for="exampleInputPassword1">Beschreibung</label>
          <input type="text" class="form-control" name="description"> <br>
+      </div> 
+
+      <div class="mb-3">
          <input type="checkbox" class="form-check-input" name="completed">
          <label class="form-check-label" for="exampleCheck1">Bereits erledigt</label> <br>
-         <input type="submit" value="Hinzufügen" class="btn btn-primary"></input>
+      </div>
+
+      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+       <input type="submit" value="Hinzufügen" class="btn btn-primary me-md-2"></input>
+       <a href="/dashboard" class="btn btn-danger">Zurück </a>
+      </div>
+
       </form>
+      </div>
    </body>
 </html>
--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Aufgaben bearbeiten</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
-<body style="text-align:center">
+<body>
+<div class="container">
     <h1>Aufgaben bearbeiten</h1>
     <h3>
         <x-alert />
@@ -12,17 +15,29 @@
     <form action="/update" method="post">
         @csrf
         @method('PUT')
-        <label for="exampleInputEmail1">Titel</label>
-        <input type="text" name="title" value="{{$todo->title}}"/> <br>
-        <input style="display:none" type="number" name="id" value="{{$todo->id}}">
-        <label for="exampleInputEmail1">Beschreibung</label>
-        <input type="text" name="description" value="{{$todo->description}}"/> <br>
-        <input type="checkbox" class="form-check-input" name="completed">
-        <label class="form-check-input" type="checkbox" value="1">Bereits erledigt</label> <br>
+
+        <div class="mb-3">
+            <label for="exampleInputEmail1">Titel</label>
+            <input type="text" class="form-control" name="title" value="{{$todo->title}}"/> <br>
+            <input style="display:none" type="number" name="id" value="{{$todo->id}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputEmail1">Beschreibung</label>
+            <input type="text" name="description" class="form-control" value="{{$todo->description}}"/> <br>
+        </div>
+
+        <div class="mb-3"> 
+             <input type="checkbox" class="form-check-input" name="completed">Bereits erledigt </input>
+             <!-- <label class="form-check-input" type="checkbox" value="1">Bereits erledigt</label> <br>-->
+        </div>
+
+    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
         <input type="submit" value="Hinzufügen" class="btn btn-primary"></input>
-        
-    </form>
-    <br>
-    <a href="/dashboard" class="btn btn-sm btn-danger">Zurück </a>
+        <a href="/dashboard" class="btn btn-sm btn-danger">Zurück </a>
+    </div>
+
+    </form>   
+    </div> 
 </body>
 </html>
